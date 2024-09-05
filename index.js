3d5519e8c6afccaa723727219ecf63f89d83f518
Sofia Noor Rafa <91686970+sofiautilitarian@users.noreply.github.com>
2024-09-05 12:11:31 +0600

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ if (n==0){
 else if (n==1){
     $(".selfformedclass1").text ("Which of the following characteristics is typical of wind-pollinated flowers?");
     $(".selfformedclass2").text ("Brightly colored petals";
-    $(".selfformedclass3").text ( "Small, inconspicuous flowers";
+    $(".selfformedclass3").text ( "Not so bright petals");
     $(".selfformedclass4").text ( "Heavy, sticky pollen";
     $(".selfformedclass5").text ( "Sweet nectar";
 }
@@ -44,115 +44,94 @@ else if (n==4){
 
 else if (n==5){
     $(".selfformedclass1").text ( "What is pollination?";
-    $(".selfformedclass2").text ( "The movement of water in plants";
-    $(".selfformedclass3").text ( "The transfer of pollen from one flower to another";
-    $(".selfformedclass4").text ( "The growth of a plant from a seed";
-    $(".selfformedclass5").text ( "The wilting of flowers";
+    $(".selfformedclass2").text ( "The movement of water in plants");
+    $(".selfformedclass3").text ( "The transfer of pollen from one flower to another");
+    $(".selfformedclass4").text ( "The growth of a plant from a seed");
+    $(".selfformedclass5").text ( "The wilting of flowers");
 }
 
 else if (n==6){
-    $(".selfformedclass1").text ( "Which of the following animals helps with pollination?";
-    $(".selfformedclass2").text ( " Elephants";
-    $(".selfformedclass3").text ( "Bees";
-    $(".selfformedclass4").text ( "Snakes";
-    $(".selfformedclass5").text ( "Fish";
+    $(".selfformedclass1").text ("Which of the following animals helps with pollination?");
+    $(".selfformedclass2").text ("Elephants");
+    $(".selfformedclass3").text ("Bees");
+    $(".selfformedclass4").text ("Snakes");
+    $(".selfformedclass5").text ("Fish");
 }
 else if (n==7){
-    $(".selfformedclass1").text ( "How do bees help with pollination?");
-    $(".selfformedclass2").text ( "By eating the leaves";
-    $(".selfformedclass3").text ( "By transferring pollen from flower to flower";
-    $(".selfformedclass4").text ( "By digging in the soil";
-    $(".selfformedclass5").text ( "By drinking water from flowers";
+    $(".selfformedclass1").text ("How do bees help with pollination?");
+    $(".selfformedclass2").text ("By eating the leaves");
+    $(".selfformedclass3").text ("By transferring pollen from flower to flower");
+    $(".selfformedclass4").text ("By digging in the soil");
+    $(".selfformedclass5").text ("By drinking water from flowers");
 }
 else if (n==8){
-    $(".selfformedclass1").text ( "Wind pollination is most effective in: ";
-    $(".selfformedclass2").text ( "Open, airy environments";
-    $(".selfformedclass3").text ( "Dense forests";
-    $(".selfformedclass4").text ( "Tropical rainforests";
-    $(".selfformedclass5").text ( "Dark, shaded areas";
+    $(".selfformedclass1").text ( "Wind pollination is most effective in: ");
+    $(".selfformedclass2").text ( "Open, airy environments");
+    $(".selfformedclass3").text ( "Dense forests");
+    $(".selfformedclass4").text ( "Tropical rainforests");
+    $(".selfformedclass5").text ( "Dark, shaded areas");
 }
 else if (n==9){
-    $(".selfformedclass1").text ( "";
-    $(".selfformedclass2").text ( "Open, airy environments";
-    $(".selfformedclass3").text ( "Dense forests";
-    $(".selfformedclass4").text ( "Tropical rainforests";
-    $(".selfformedclass5").text ( "Dark, shaded areas";
+    $(".selfformedclass1").text ( "Fruit provides :";
+    $(".selfformedclass2").text ( "Protection to seeds");
+    $(".selfformedclass3").text ( "Flavour to seeds");
+    $(".selfformedclass4").text ( "flowers to seeds";
+    $(".selfformedclass5").text ( "plants to seeds");
 }
 else if (n==10){
-    $(".selfformedclass1").text = "Wind pollination is most effective in: ";
-    $(".selfformedclass2").text ( "Open, airy environments";
-    $(".selfformedclass3").text ( "Dense forests";
-    $(".selfformedclass4").text ( "Tropical rainforests";
-    $(".selfformedclass5").text ( "Dark, shaded areas";
+    $(".selfformedclass1").text ("During pollination pollen moves from");
+    $(".selfformedclass2").text ( "Anther");
+    $(".selfformedclass3").text ( "Stigma");
+    $(".selfformedclass4").text ( "Stem");
+    $(".selfformedclass5").text ( "Leaves");
 }
 else if (n==11){
-    $(".selfformedclass1").text = "Wind pollination is most effective in: ";
-    $(".selfformedclass2").text = "Open, airy environments";
-    $(".selfformedclass3").text = "Dense forests";
-    $(".selfformedclass4").text = "Tropical rainforests";
-    $(".selfformedclass5").text = "Dark, shaded areas";
+    $(".selfformedclass1").text('plants can produce offspring is by making');
+    $(".selfformedclass2").text('seeds);
+    $(".selfformedclass3").text('leaves');
+    $(".selfformedclass4").text(sunlight);
+    $(".selfformedclass5").text('root');
 }
 else if (n==12){
-    $(".selfformedclass1").text = "Wind pollination is most effective in: ";
-    $(".selfformedclass2").text = "Open, airy environments";
-    $(".selfformedclass3").text = "Dense forests";
-    $(".selfformedclass4").text = "Tropical rainforests";
-    $(".selfformedclass5").text = "Dark, shaded areas";
+    $(".selfformedclass1").text("Flowers getting pollinated by insects have");
+    $(".selfformedclass2").text("bright, colourful petals");
+    $(".selfformedclass3").text("no so bright petals");
+    $(".selfformedclass4").text("no nectar");
+    $(".selfformedclass5").text("lightweight pollens");
 }
 else if (n==13){
-    $(".selfformedclass1").text = "Wind pollination is most effective in: ";
-    $(".selfformedclass2").text = "Open, airy environments";
-    $(".selfformedclass3").text = "Dense forests";
-    $(".selfformedclass4").text = "Tropical rainforests";
-    $(".selfformedclass5").text = "Dark, shaded areas";
+    $(".selfformedclass1").text("Pollination is an essential part of:");
+    $(".selfformedclass2").text("plant death");
+    $(".selfformedclass3").text("plant enlargement");
+    $(".selfformedclass4").text("plant reproduction");
+    $(".selfformedclass5").text("plant strength");
 }
 else if (n==14){
-    $(".selfformedclass1").text = "Wind pollination is most effective in: ";
-    $(".selfformedclass2").text = "Open, airy environments";
-    $(".selfformedclass3").text = "Dense forests";
-    $(".selfformedclass4").text = "Tropical rainforests";
-    $(".selfformedclass5").text = "Dark, shaded areas";
+    $(".selfformedclass1").text("Insects or wind are";
+    $(".selfformedclass2").text("pollinators");
+    $(".selfformedclass3").text("seeds");
+    $(".selfformedclass4").text("influencers");
+    $(".selfformedclass5").text("animals");
 }
 else if (n==15){
-    $(".selfformedclass1").text = "Wind pollination is most effective in: ";
-    $(".selfformedclass2").text = "Open, airy environments";
-    $(".selfformedclass3").text = "Dense forests";
-    $(".selfformedclass4").text = "Tropical rainforests";
-    $(".selfformedclass5").text = "Dark, shaded areas";
+    $(".selfformedclass1").text("flowers pollinated by insects have";
+    $(".selfformedclass2").text("large nectar glands");
+    $(".selfformedclass3").text("Dense forests");
+    $(".selfformedclass4").text("Tropical rainforests");
+    $(".selfformedclass5").text("Dark, shaded areas");
 }
 else if (n==16){
-    $(".selfformedclass1").text = "Wind pollination is most effective in: ";
-    $(".selfformedclass2").text = "Open, airy environments";
-    $(".selfformedclass3").text = "Dense forests";
-    $(".selfformedclass4").text = "Tropical rainforests";
-    $(".selfformedclass5").text = "Dark, shaded areas";
+    $(".selfformedclass1").text("Flowers pollinated by insects have";
+    $(".selfformedclass2").text("Great smell");
+    $(".selfformedclass3").text("Dense forests");
+    $(".selfformedclass4").text("Tropical rainforests");
+    $(".selfformedclass5").text("Dark, shaded areas");
 }
 else if (n==17){
-    $(".selfformedclass1").text = "Wind pollination is most effective in: ";
-    $(".selfformedclass2").text = "Open, airy environments";
-    $(".selfformedclass3").text = "Dense forests";
-    $(".selfformedclass4").text = "Tropical rainforests";
-    $(".selfformedclass5").text = "Dark, shaded areas";
-}
-else if (n==18){
-    $(".selfformedclass1").text = "Wind pollination is most effective in: ";
-    $(".selfformedclass2").text = "Open, airy environments";
-    $(".selfformedclass3").text = "Dense forests";
-    $(".selfformedclass4").text = "Tropical rainforests";
-    $(".selfformedclass5").text = "Dark, shaded areas";
-}
-else if (n==19){
-    $(".selfformedclass1").text = "Wind pollination is most effective in: ";
-    $(".selfformedclass2").text = "Open, airy environments";
-    $(".selfformedclass3").text = "Dense forests";
-    $(".selfformedclass4").text = "Tropical rainforests";
-    $(".selfformedclass5").text = "Dark, shaded areas";
-}
-else if (n==20){
-    $(".selfformedclass1").text = "Wind pollination is most effective in: ";
-    $(".selfformedclass2").text = "Open, airy environments";
-    $(".selfformedclass3").text = "Dense forests";
-    $(".selfformedclass4").text = "Tropical rainforests";
-    $(".selfformedclass5").text = "Dark, shaded areas";
+    $(".selfformedclass1").text("Based on flowering, plants can be of :");
+    $(".selfformedclass2").text("4 types");
+    $(".selfformedclass3").text("6 types");
+    $(".selfformedclass4").text("2 types");
+    $(".selfformedclass5").text("3 types");
 }
 
